056aaca73f53175b9ac21b8d0f90f2a301ea6b43
shravan97 <shravanmurali@gmail.com>
2015-12-13 02:28:41 +0530

    added data

--- ph.php
+++ ph.php
@@ -1,126 +1,49 @@
 <?php 
-<<<<<<< HEAD
 include 'db_connect.php';
 session_start();
 
+$id=$_SESSION['u_i'];
+	$q = "SELECT `type` from user_details where u_id = $id;";
+	$r = mysqli_query($h,$q) or die("Error....");
+	$arr_T = mysqli_fetch_array($r);
+	$type = $arr_T['type'];
 if(!isset($_SESSION['u_i']))
 	header("Location:/meditrack/land.html");
 
 
 if(isset($_POST['submit']))
 {
-	$fname = mysqli_real_escape_string($h , $_POST['fname']);
+	$uname = mysqli_real_escape_string($h , $_POST['uname']);
 	$date = mysqli_real_escape_string($h , $_POST['dt'] );
 
 	$s_level = mysqli_real_escape_string($h ,$_POST['sl']);
 	$bp = mysqli_real_escape_string($h , $_POST['bp']);
-	$d = mysqli_real_escape_string($h ,$_POST['disease']);
+	$d = mysqli_real_escape_string($h ,$_POST['disease_desc']);
 	$symptoms = mysqli_real_escape_string($h , $_POST['symp']);
-	$id=$_SESSION['u_i'];
+	$cmt = mysqli_real_escape_string($h , $_POST['cmts']);
 
-	$q_uname = "SELECT `uname` , `imgs_uploaded` from user_details where u_id=$id";
+	
+	$q_uname = "SELECT `u_id`  , `imgs_uploaded` from user_details where uname='$uname';";
 	$res = mysqli_query($h , $q_uname);
 	$arr = mysqli_fetch_array($res);
-	$filename = $arr['uname'].'_'.(string)($arr['imgs_uploaded']+1);
+	$filename = $uname.'_'.(string)($arr['imgs_uploaded']+1).'.png';
+	$imgs = $arr['imgs_uploaded']+1;
 
-	move_uploaded_file($_FILES['tmp_name'], $_SERVER['DOCUMENT_ROOT'].'/meditrack/uploads/'.$filename);
+	$query_img = "UPDATE user_details set imgs_uploaded=$imgs where uname='$uname';";
+	$res_img = mysqli_query($h,$query_img) or die("error.."); 
+
+	$p_id = mysqli_real_escape_string($h,$arr['u_id']);
+	
+	$query = "INSERT INTO patient_history values($p_id , $id , NOW(),$s_level , $bp , '$d' , '$symptoms' ,'$cmt' , $imgs);";
+	$res = mysqli_query($h,$query);
+	$path = $_SERVER['DOCUMENT_ROOT'].'/meditrack/uploads/'.$filename;
+	
+	move_uploaded_file($_FILES['upload_file']['tmp_name'], $path);
+
 	echo "Done uploading successfully!";
 }
 ?>
-<!DOCTYPE HTML>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>MEDI TRACK</title>
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/landing-page.css" rel="stylesheet">
-		<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
-		<script src="js/jquery.js"></script>
-		<script src="js/bootstrap.js"></script>
-	</head>
 
-	<body style="overflow:hidden">
-			<div id="login" class="text-center">
-			<a class="btn btn-primary" role="button" data-toggle="collapse" href="#form" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-heart"> ADD PATIENT HISTORY </i></a>
-			<div class="collapse" id="form" aria-expanded="false">
-			<form id="form" method="post" action="ph.php">
-			<div class="form-group">
-				<label>
-					<span>Name: </span>
-					<input placeholder="Please enter your full name" type="text" name="fname" class="form-control" tabindex="1" autofocus required>
-				</label>
-			</div>
-			<div class="form-group">
-				<label>
-					<span>Date: </span>
-					<input type="date" name="dt" tabindex="2" required>
-				</label>
-			</div>
-			<div class="form-group">
-				<label>
-					<span>Sugar Level: </span>
-					<input type="number" name="sl" class="form-control" tabindex="3" required>
-				</label>
-			</div>
-			<div class="form-group">
-				<label>
-					<span>BP Level</span>
-					<input type="number" name="bp" class="form-control" tabindex="6" required>
-				</label>
-			</div>
-			<div class="form-group">
-				<label>
-					<span>Disease: </span>
-					<textarea name="disease" class="form-control" tabindex="7" required></textarea>
-				</label>
-			</div>
-			<div class="form-group">
-				<label>
-					<span>Symptoms: </span>
-					<textarea name="symp" class="form-control" tabindex="7" required></textarea>
-				</label>
-			</div>
-			<label class="file">
-			<input type="file" id="file" name="file_upload">
-			<span class="file-custom"></span>
-			</label>
-				<button name="submit" type="submit" class="btn btn-primary" id="submit">Submit</button>
-			</form>
-			</div>
-			<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-  <div class="panel panel-default">
-    <div class="panel-heading" role="tab" id="headingOne">
-      <h4 class="panel-title">
-        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Collapsible Group Item #1
-        </a>
-      </h4>
-    </div>
-    <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
-      <div class="panel-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-=======
-   include 'db_connect.php';
-   if(isset($_POST['submit'])){
-   	$uname = $_POST['uname'];
-   	$fname = $_POST['fname'];
-   	$pwd = sha1($_POST['pwd']);
-   	$dob = $_POST['dt'];
-   	$ph_no = $_POST['ph'];
-   	$email = $_POST['email'];
-   	$addr = $_POST['addr'];
-   	$type = $_POST['type'];
-   	$query = "INSERT INTO user_details(`uname` , `full_name` , `pwd` , `dob` , `ph_no` , `address` ,`email` ,`type`) VALUES('$uname' , '$fname' , '$pwd' , '$dob' ,$ph_no , '$addr' , '$email' , {$type});";
-   
-   
-   	$res = mysqli_query($h , $query) or die("Error ...".mysqli_error($h)) ;
-   	if(mysqli_error($h)=='')
-   	echo 'Thank you for registering ! Please Click 
-   <a href="login.php">Here</a> to login';	
-   }
-   
-   ?>
 <!DOCTYPE HTML>
 <html>
    <head>
@@ -135,15 +58,16 @@ if(isset($_POST['submit']))
    </head>
    <body>
       <div id="login" class="text-center">
+         <?php if($type!=3){ ?>
          <a class="btn btn-primary" role="button" data-toggle="collapse" href="#form" aria-expanded="false" aria-controls="collapseExample">
          <i class="fa fa-heart"> ADD PATIENT HISTORY </i>
          </a>
          <div class="collapse" id="form" aria-expanded="false">
-            <form id="form" method="post" action="register.php">
+            <form id="form" method="post" action="ph.php" enctype="multipart/form-data">
                <div class="form-group">
                   <label>
-                  <span>Name: </span>
-                  <input placeholder="Please enter your full name" type="text" name="fname" class="form-control" tabindex="1" autofocus required>
+                  <span>uname: </span>
+                  <input placeholder="Please enter your full name" type="text" name="uname" class="form-control" tabindex="1" autofocus required>
                   </label>
                </div>
                <div class="form-group">
@@ -155,34 +79,41 @@ if(isset($_POST['submit']))
                <div class="form-group">
                   <label>
                   <span>Sugar Level: </span>
-                  <input type="number" name="ph" class="form-control" tabindex="3" required>
+                  <input type="number" name="sl" class="form-control" tabindex="3" required>
                   </label>
                </div>
                <div class="form-group">
                   <label>
                   <span>BP Level</span>
-                  <input type="email" name="email" class="form-control" tabindex="6" required>
+                  <input type="number" name="bp" class="form-control" tabindex="6" required>
                   </label>
                </div>
                <div class="form-group">
                   <label>
-                  <span>Disease: </span>
-                  <textarea name="addr" class="form-control" tabindex="7" required></textarea>
+                  <span>Disease Description: </span>
+                  <textarea name="disease_desc" class="form-control" tabindex="7" required></textarea>
                   </label>
                </div>
                <div class="form-group">
                   <label>
                   <span>Symptoms: </span>
-                  <textarea name="addr" class="form-control" tabindex="7" required></textarea>
+                  <textarea name="symp" class="form-control" tabindex="8" required></textarea>
+                  </label>
+               </div>
+               <div class="form-group">
+                  <label>
+                  <span>Comments: </span>
+                  <textarea name="cmts" class="form-control" tabindex="9" required></textarea>
                   </label>
                </div>
                <label class="file">
-               <input type="file" id="file">
+               <input type="file" id="file" name="upload_file" accept="image/gif,image/jpeg,image/jpg,image/png">
                <span class="file-custom"></span>
                </label>
                <button name="submit" type="submit" class="btn btn-primary" id="submit">Submit</button>
             </form>
          </div>
+         <?php }?>
          <ul class="nav nav-pills nav-justified">
             <li role="presentation" class="active">
                <a href="#atr" class="btn btn-primary" role="button" data-toggle="collapse" aria-expanded="false" aria-controls="atr">By Attribute</a>
@@ -256,7 +187,6 @@ if(isset($_POST['submit']))
             </li>
             </div>
          </ul>
->>>>>>> 1e043253e015311c32c4ae200a2ad764a5d7ec94
       </div>
    </body>
 </html>
